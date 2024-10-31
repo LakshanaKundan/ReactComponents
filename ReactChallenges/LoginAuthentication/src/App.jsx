3992097dcfaa5db88bcda113c0275a1fd4727b40
LakshanaKundan <lakshana.vfd176@gmail.com>
2024-10-30 22:46:18 -0400

    initializing Login component

---
+++ ReactChallenges/LoginAuthentication/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './App.css'
+import Login from './login'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='App'> 
+      <Login/>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ ReactChallenges/LoginAuthentication/src/login.js
@@ -0,0 +1,11 @@
+import React, {useState} from "react";
+const mockUser = {
+    email: 'user@example.com',
+    password: 'password@123'
+};
+
+function Login() {
+    const [email,setEmail] = useState('');
+}
+
+export default Login
