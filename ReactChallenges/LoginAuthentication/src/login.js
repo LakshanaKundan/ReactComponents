d50f0446e1e3bf22f0b73d3d4e42f46c9fe89cae
LakshanaKundan <lakshana.vfd176@gmail.com>
2024-10-31 23:49:03 -0400

    added handling fucntions

--- ReactChallenges/LoginAuthentication/src/login.js
+++ ReactChallenges/LoginAuthentication/src/login.js
@@ -6,6 +6,55 @@ const mockUser = {
 
 function Login() {
     const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+    const [error,setError] = useState('');
+    const [success,setSuccess] = useState('');
+
+
+
+
+    const handleLogin = () => {
+        if(email === mockUser.email && password === mockUser.password){
+            setSuccess('Login Successful');
+            setError('');
+        } else{
+            setError('Invalid email or password');
+            setSuccess('');
+        }
+    };
+
+
+    const validateForm = () => {
+        if(!email || !password){
+            setError('Both fields are required');
+            return false;
+        }
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+        setError('Please enter a valid email');
+        return false;
+        }
+        return true;
+    };
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        if (validateForm()){
+            handleLogin();
+        }
+    };
+
+
+    return (
+        <div style={{maxWidth: '400px', margin: 'auto', padding:'1em'}}>
+            
+        </div>
+    )
+
 }
+
+
+
+
 
 export default Login
